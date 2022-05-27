ebca8e5e53b5281453e603ec0a622786de7defc4
Alvaro Japa Salazar <alvaruwiiu@hotmail.com>
2022-05-26 21:03:04 -0500

    project lamp finished

---
+++ src/components/IndividualLamp.jsx
@@ -0,0 +1,19 @@
+//rafce snip para crear un componente
+
+import React from 'react'
+import Lamp from './Lamp'
+
+const IndividualLamp = () => {
+  return (
+    <div className='individual-lamp'>
+        <Lamp />
+        <Lamp />
+        <Lamp />
+        <Lamp />
+        <Lamp />
+        <Lamp />
+    </div>
+  )
+}
+
+export default IndividualLamp
---
+++ src/components/Lamp.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+
+const Lamp = () => {
+
+    const [isOn, setIsOn] = useState('on')
+
+    const switchLight = () => {
+        if (isOn === 'on') {
+            setIsOn('off')
+        } else {
+            setIsOn('on')
+        }
+    }
+
+    return (
+
+        <div className='lamp-and-btn'>
+            <div className={`lamp ${isOn}`}></div>
+            <button onClick={switchLight} className='lamp-btn'>{isOn}</button>
+        </div>
+
+    )
+}
+
+export default Lamp
